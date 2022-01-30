41040860bc73fd044c086febf4cce8559ec28e3a
MichaelXi3 <92639450+MichaelXi3@users.noreply.github.com>
2022-01-30 16:02:58 -0500

    1

--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,102 @@
+import java.util.Random;
 public class Hero {
+    // Private variables
+    private String heroName;
+    private int hitPoints;
+    private int lightAttackDamage;
+    private int heavyAttackDamage;
+    private int thisTimeAttack;
 
-    //FIXME
+    //Default constructor
+    public Hero(String s, int i, int parseInt, int anInt, int i1) {
+        this.heroName = "That's an error, wrong contructor";
+        this.hitPoints = 1000;
+        this.lightAttackDamage = 100;
+        this.heavyAttackDamage = 200;
+    }
+
+    // Constructor with parameter
+    public Hero (String heroName, int hitPoints, int lightAttackDamage, int heavyAttackDamage ) {
+        this.heroName = heroName;
+        this.hitPoints = hitPoints;
+        this.lightAttackDamage = lightAttackDamage;
+        this.heavyAttackDamage = heavyAttackDamage;
+    }
+
+    //Mutators
+    public void setHeroName (String heroName) {
+        this.heroName = heroName;
+    }
+    public void sethitPoints (int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+    public void setlightAttackDamage (int lightAttackDamage) {
+        this.lightAttackDamage = lightAttackDamage;
+    }
+    public void setheavyAttackDamage (int heavyAttackDamage) {
+        this.heavyAttackDamage = heavyAttackDamage;
+    }
+    public void setThisTimeAttack (int thisTimeAttack) {
+        this.thisTimeAttack = thisTimeAttack;
+    }
+
+    //Accessor
+    public String getHeroName () {
+        return heroName;
+    }
+    public int gethitPoints () {
+        return hitPoints;
+    }
+    public int getlightAttackDamage () {
+        return lightAttackDamage;
+    }
+    public int getheavyAttackDamage() {
+        return heavyAttackDamage;
+    }
+    public int getThisTimeAttack() {
+        return thisTimeAttack;
+    }
+
+    //isAlive method
+    public boolean isLive () {
+        if (hitPoints > 0) {
+            return true;
+        } else { return false; }
+    }
+
+    //Attack method
+    public int attack () {
+        Random ran = new Random();
+        int prob = ran.nextInt(4) + 1;
+        if (prob == 1 || prob == 2 || prob == 3) {
+            return lightAttackDamage;
+        } else { return heavyAttackDamage; }
+    }
+
+    public void displayAttack (int attackAmount) {
+        if (attackAmount == lightAttackDamage) {
+            System.out.println(heroName + " performs a light Attack with Damage: " + lightAttackDamage);
+            thisTimeAttack = lightAttackDamage;
+        } if (attackAmount == heavyAttackDamage) {
+            System.out.println(heroName + " performs a heavy Attack with Damage: " + heavyAttackDamage);
+            thisTimeAttack = heavyAttackDamage;
+        }
+    }
+
+    public void takeDamage (int damageTaken) {
+        hitPoints = hitPoints - damageTaken;
+    }
+
+    public void displayHealth () {
+        if (hitPoints > 0) {
+            System.out.println(heroName + " has " + hitPoints + " HP left");
+        } if (hitPoints <= 0 ) {
+            System.out.println(heroName + " has " + "fainted");
+        }
+    }
+
+
+
+
+
 }
--- src/Lab2.java
+++ src/Lab2.java
@@ -1,7 +1,107 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2 {
+    public static void fightComplexVersion (ArrayList<Hero> team1, ArrayList<Hero> team2, int round) {
+        System.out.println(team1.get(round).getHeroName() + " VS. " + team2.get(round).getHeroName());
+        System.out.println("============================================");
+        while (team1.get(round).isLive() && team2.get(round).isLive()) {
+            //Display health
+            team1.get(round).displayHealth();
+            team2.get(round).displayHealth();
+            System.out.println("============================================");
+            //Perform attack
+            team1.get(round).displayAttack(team1.get(round).attack()); team2.get(round).takeDamage(team1.get(round).getThisTimeAttack());
+            team2.get(round).displayAttack(team2.get(round).attack()); team1.get(round).takeDamage(team2.get(round).getThisTimeAttack());
+            System.out.println("============================================");
+        }
+        if (team1.get(round).gethitPoints() < 0) {
+            team1.get(round).displayHealth();
+            team2.get(round).displayHealth();
+            System.out.println("============================================");
+            System.out.println(team2.get(round).getHeroName() + " VANQUISHES " + team1.get(round).getHeroName() + " with " + team2.get(round).gethitPoints() + " HP to spare");
+        } else {
+            team2.get(round).displayHealth();
+            team1.get(round).displayHealth();
+            System.out.println("============================================");
+            System.out.println(team1.get(round).getHeroName() + " VANQUISHES " + team2.get(round).getHeroName() + " with " + team1.get(round).gethitPoints() + " HP to spare");
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void fightSimpleVersion (ArrayList<Hero> team1, ArrayList<Hero> team2, int round) {
+        System.out.println(team1.get(round).getHeroName()+" " + " VS. " + team2.get(round).getHeroName());
+        while (team1.get(round).gethitPoints()>0 && team2.get(round).gethitPoints()>0) {
+            team2.get(round).takeDamage(team1.get(round).attack());
+            if (team2.get(round).gethitPoints()<0) {break;}
+            team1.get(round).takeDamage(team2.get(round).attack());
+        }
+        if (team1.get(round).isLive()) {
+            System.out.println(team1.get(round).getHeroName() + " VANQUISHES " + team2.get(round).getHeroName() + " with " + team1.get(round).gethitPoints() + " HP to spare");
+        } if (team2.get(round).isLive()) {
+            System.out.println(team2.get(round).getHeroName() + " VANQUISHES " + team1.get(round).getHeroName() + " with " + team2.get(round).gethitPoints() + " HP to spare");
+        }
+    }
 
-        System.out.println("FIXME");
+    public static void fightBetweenTeamsPrint (ArrayList<Hero> team1, ArrayList<Hero> team2, boolean display, int round) {
+        if (!display) { fightSimpleVersion(team1, team2, round); }
+        if (display) { fightComplexVersion(team1, team2, round); }
+    }
+
+    public static void doBattle (ArrayList<Hero> team1, ArrayList<Hero> team2, boolean flag) {
+        //System.out.println(flag[0]);
+        for (int i=0; i< team1.size(); i++) {
+            fightBetweenTeamsPrint(team1, team2, flag, i);
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //read flag boolean and check
+        try {
+            if (!args[0].equals("false") && !(args[0].equals("true"))) {
+                throw new Exception ("the print flag is incorrectly set");}
+        } catch (Exception excpt) {
+            System.out.println(excpt.getMessage());
+            System.exit(1);
+        }
+        boolean display = Boolean.parseBoolean(args[0]);
+
+        //log in the file content
+        Scanner scan = new Scanner(System.in);
+        boolean getCorrectInput = false;
+        FileInputStream heroFile = null;
+        String fileName = null;
+        while (!getCorrectInput) {
+            try {
+                System.out.println("Enter file name:");
+                fileName = scan.nextLine();
+                heroFile = new FileInputStream("src/" + fileName);
+                getCorrectInput = true;
+            } catch (FileNotFoundException e) {
+                System.out.println("Could not open input file. Enter another file name:");
+                continue;
+            }
+        }
+        //Read the file and create two teams
+        Scanner fileReader = new Scanner(heroFile);
+        ArrayList <Hero> team1 = new ArrayList<Hero>();
+        ArrayList <Hero> team2 = new ArrayList<Hero>();
+        int count = 0;
+        while (fileReader.hasNextLine()) {
+            String data = fileReader.nextLine(); //reading lines in file
+            String[] eachHeroInfo = data.split(",");
+            count++;
+            //Put eachHeroInfo into Arraylist
+            try {
+                if (Integer.parseInt(eachHeroInfo[4]) == 1) {
+                    team1.add(new Hero((eachHeroInfo[0]), Integer.parseInt(eachHeroInfo[1]), Integer.parseInt(eachHeroInfo[2]), Integer.parseInt(eachHeroInfo[3])));}
+                if (Integer.parseInt(eachHeroInfo[4]) == 2) {
+                    team2.add(new Hero((eachHeroInfo[0]), Integer.parseInt(eachHeroInfo[1]), Integer.parseInt(eachHeroInfo[2]), Integer.parseInt(eachHeroInfo[3])));}
+            } catch (NumberFormatException e) {continue;}
+        }
+        fileReader.close();
+
+        doBattle(team1, team2, display);
     }
 }
